0fb5b2cfe1d05d669b467fe665b0e65ee0c8d604
Livne, Alex <alex.livne@striders.io>
2025-08-10 20:14:36 +0300

    Add Kubernetes YAML manifests

--- HW.py
+++ HW.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello():
     return "Hello and welcome to Alexandra's Docker Project"
 
+@app.route("/health")
+def health():
+    return jsonify(status="ok"), 200
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5050, debug=True)
+
